4d6c4c18f73bf9ddff771e7b54f288830bcfca45
Shubham Yadav <yadavshubham0711@gmail.com>
2024-06-25 11:32:44 +0530

    "Folder structure updated"

---
+++ netlify/functions/json-server.js
@@ -0,0 +1,35 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./netlify/functions/json-server/db.json');
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+
+// const handler = async (event, context) => {
+//   const response = await new Promise((resolve, reject) => {
+//     server.handle(event, context, (err, res) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(res);
+//       }
+//     });
+//   });
+//   return response;
+// };
+
+// // Export the handler function directly
+// module.exports.handler = handler;
+
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = server;
